7f557434893d937a041490986b4db6351c36a797
Shobhit Dixit <shobhit-m@github.com>
2016-06-20 11:13:50 +0530

    added 255 characters limit to string

--- db/migrate/20160615074820_create_meta_notification_notification_types.rb
+++ db/migrate/20160615074820_create_meta_notification_notification_types.rb
@@ -1,8 +1,8 @@
 class CreateMetaNotificationNotificationTypes < ActiveRecord::Migration
   def change
     create_table :mn_notification_types do |t|
-      t.string :name, null: false, unique: true
-      t.string :display_name
+      t.string :name, null: false, unique: true, limit: 255
+      t.string :display_name, limit: 255
       t.references :created_by, null: false
       t.references :updated_by, null: false
 
