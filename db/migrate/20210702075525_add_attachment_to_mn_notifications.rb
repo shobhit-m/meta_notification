a473e73a416a2007226490d5962d5f3eb368ed52
meta-namrata-agarwal <namrata.agarwal@metacube.com>
2021-07-02 14:39:11 +0530

    changes

--- db/migrate/20210702075525_add_attachment_to_mn_notifications.rb
+++ db/migrate/20210702075525_add_attachment_to_mn_notifications.rb
@@ -1,5 +1,5 @@
 class AddAttachmentToMnNotifications < ActiveRecord::Migration[5.0]
   def change
-    add_column :mn_notifications, :attatchment, :string
+    add_column :mn_notifications, :attachment, :string
   end
 end
